74e9589a0e83a8850385ac3e746378f8b9400d92
Giacomo <gmires@virgilio.it>
2025-03-03 18:30:35 +0100

    init commit

---
+++ lp.interpreter/LPInterpreter.dpr
@@ -0,0 +1,20 @@
+program LPInterpreter;
+
+uses
+  Forms,
+  lpi.main in 'lpi.main.pas' {LPMain},
+  lp.builtins in '..\lp.lib\lp.builtins.pas',
+  lp.environment in '..\lp.lib\lp.environment.pas',
+  lp.evaluator in '..\lp.lib\lp.evaluator.pas',
+  lp.lexer in '..\lp.lib\lp.lexer.pas',
+  lp.parser in '..\lp.lib\lp.parser.pas',
+  lp.token in '..\lp.lib\lp.token.pas';
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TLPMain, LPMain);
+  Application.Run;
+end.
